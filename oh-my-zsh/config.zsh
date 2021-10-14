23083bc72f1369b385986280e4c841b47ab3c969
Adam Jenkins <adam.jenkins@zoro.com>
2021-10-14 11:59:03 -0500

    Converts ZSH_CUSTOM to submodules

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -1,7 +1,18 @@
-ZSH_THEME="powerlevel10k/powerlevel10k"
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+export ZSH_THEME="powerlevel10k/powerlevel10k"
 
-# plugins=(docker docker-compose git kubectl z)
-plugins=(git node npm zsh-completions zsh-syntax-highlighting zsh-nvm docker docker-compose)
+# Would you like to use another custom folder than $ZSH/custom?
+export ZSH_CUSTOM="$DOT/oh-my-zsh/custom"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git node npm zsh-completions zsh-syntax-highlighting zsh-nvm zsh-autosuggestions alias-tips)
 
 # Load oh-my-zsh
 source $ZSH/oh-my-zsh.sh
